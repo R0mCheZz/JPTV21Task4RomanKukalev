64e462f7c8063ae513e11f7bb428aa05ac6e003e
pupil <pupil@ivkhk.ee>
2022-09-26 15:15:38 +0300

    Done

--- src/jptv21task4romankukalev/JPTV21Task4RomanKukalev.java
+++ src/jptv21task4romankukalev/JPTV21Task4RomanKukalev.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package jptv21task4romankukalev;
-
+import java.util.Arrays;
+import java.util.Random;
 /**
  *
  * @author pupil
@@ -15,6 +16,31 @@ public class JPTV21Task4RomanKukalev {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+            Random random = new Random();
+        int n = 101;
+        int sum = 0;
+        System.out.println("Одномерные Массивы");
+        int[] nums = new int[n];
+        for (int i = 0; i < nums.length; i++) {
+            while(true){
+                int rundNum = random.nextInt(100);
+                if (rundNum % 2 != 0){
+                    nums[i] = rundNum;
+                    break;
+                }
+            }
+        }
+        Arrays.sort(nums);    
+        for (int i = 1; i < nums.length; i++) {
+            System.out.printf("%-4d", nums[i]);
+            sum += nums[i];  
+            if (i%10==0){
+                System.out.println("");
+            }
+                
+        }
+        System.out.println();
+        System.out.println("Среднее арифметическое: " + sum/n);
         // TODO code application logic here
     }
     
